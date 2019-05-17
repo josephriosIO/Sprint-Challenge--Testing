c6268d10c9f0f3be7555eb7ff69bdef5b75486b0
josephriosio <blankworldwide@gmail.com>
2019-05-17 09:48:19 -0700

    completed get endpoint tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-
+const db = require("../database/dbConfig");
 const server = require("./server");
 
 describe("Server.js", () => {
@@ -9,5 +9,25 @@ describe("Server.js", () => {
 
       expect(res.status).toBe(200);
     });
+  });
+  describe("gamesRouter.js", () => {
+    afterEach(async () => {
+      await db("games").truncate();
+    });
+    describe("GET ENDPOINT", () => {
+      it("receives a 200 status code when recieving right data", async () => {
+        const res = await request(server).get("/api/games");
+        expect(res.status).toBe(200);
+      });
+      it("it should return application/json", async () => {
+        const res = await request(server).get("/api/games");
+        expect(res.type).toBe("application/json");
+      });
+      it("it should return empty array if no games are there", async () => {
+        const res = await request(server).get("/api/games");
+        expect(res.body).toHaveLength(0);
+      });
+    });
+    describe("POST ENDPOINT", () => {});
   });
 });
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
+const db = require("../database/helpers/gamesModel");
+
+router.get("/", async (req, res) => {
+  try {
+    const allGames = await db.get();
+    res.status(200).json(allGames);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
